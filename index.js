d9344405b6452ab422a33f018fd01eefba2d9e8f
Ray <raysau389@gmail.com>
2026-09-21 19:59:06 +0800

    updated index.js to use ES modules and  initialized the server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+const app = express();
+
+// Tells the server to parse incoming JSON data
+app.use(express.json());
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+);
